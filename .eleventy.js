7bfc52e56705ba113ffd614902a8d70fb3255a12
Sarah J Dawns <123122262+sarahJDawns@users.noreply.github.com>
2023-04-29 13:06:53 -0600

    setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,34 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [300, 800, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./public/images/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/images/");
   eleventyConfig.addPassthroughCopy("src/css/");
   eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site",
+      output: "public",
     },
     templateFormats: ["njk", "md", "html"],
     markdownTemplateEngine: "njk",
