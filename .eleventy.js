b4d355cf372d4c28956285e2d2d95732431c8395
Sarah J Dawns <123122262+sarahJDawns@users.noreply.github.com>
2023-05-05 18:09:49 -0600

    image

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,11 @@
 const { DateTime } = require("luxon");
-const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 const charts = require("eleventy-charts");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
-  eleventyConfig.addPlugin(lazyImagesPlugin);
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addPlugin(charts);
   eleventyConfig.addPassthroughCopy("src/assets/images/");
   eleventyConfig.addPassthroughCopy("src/assets/css/");
@@ -15,21 +14,11 @@ module.exports = function (eleventyConfig) {
     excerpt: true,
     excerpt_separator: "<!--more-->",
   });
-  eleventyConfig.setTemplateFormats([
-    "md",
-    "njk",
-    "jpg",
-    "png",
-    "webp",
-    "ico",
-    "svg",
-  ]);
   eleventyConfig.setBrowserSyncConfig({
     files: ["public/**/*"],
     open: true,
   });
   eleventyConfig.setDataDeepMerge(true);
-  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/posts/**/*.md");
   });
--- src/posts/2023/may/fifth.md
+++ src/posts/2023/may/fifth.md
@@ -6,9 +6,6 @@ date: 2023-06-15
 author: sarah j dawns
 tags:
   - worms
-image:
-  url: /src/assets/images/favicon.png
-  alt: star
 ---
 
 lksdfj;alkjfs; alskdjfa;lskdjfa;lskdf;alksd;jfa;sdlkjf;alksjd;flaj;sldfja;lskdjf;alskjd;falks
